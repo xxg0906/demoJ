c932556346f14d1b34c1387d7ae2f18804dab84c
许兴广 <arrow_xu@sinobasedm.com>
2021-06-01 11:34:47 +0800

    jwt个性化解析

--- modules/system/src/main/java/top/xxgo/config/JwtTokenExtractor.java
+++ modules/system/src/main/java/top/xxgo/config/JwtTokenExtractor.java
@@ -1,10 +1,15 @@
 package top.xxgo.config;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.authentication.BearerTokenExtractor;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.security.oauth2.provider.authentication.TokenExtractor;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 /**
  *
@@ -14,7 +19,16 @@ import javax.servlet.http.HttpServletRequest;
 public class JwtTokenExtractor  implements TokenExtractor {
     @Override
     public Authentication extract(HttpServletRequest request) {
-        BearerTokenExtractor bearerTokenExtractor = new BearerTokenExtractor();
-        return bearerTokenExtractor.extract(request);
+        String token = request.getHeader("access_token");
+        if (token != null) {
+            return new PreAuthenticatedAuthenticationToken(token, "");
+        } else {
+            token = request.getParameter(OAuth2AccessToken.ACCESS_TOKEN);
+            if (token != null) {
+                return new PreAuthenticatedAuthenticationToken(token, "");
+            }
+        }
+        return null;
+
     }
 }
